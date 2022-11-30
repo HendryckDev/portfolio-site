3618ec8ec17f44501250b27f8a1ac156918058a7
Hendryck Lima <86539955+HendryckDev@users.noreply.github.com>
2022-11-29 21:35:03 -0300

    responsive website

--- js/main.js
+++ js/main.js
@@ -19,3 +19,46 @@ changeThemeBtn.addEventListener('click', function() {
     document.body.classList.toggle('light');
     document.body.classList.toggle('icons-bg-light');
 });
+
+// =========== MOBILE MENU ===========
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = 'active';
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation 
+            ? (link.style.animation = '') 
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavBar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li',
+);
+mobileNavbar.init();
