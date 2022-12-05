4a7668ae26ec03393ef2718f6c5817d5619e1b8b
Hendryck Lima <86539955+HendryckDev@users.noreply.github.com>
2022-12-05 15:05:42 -0300

    animated website

--- js/main.js
+++ js/main.js
@@ -62,3 +62,61 @@ const mobileNavbar = new MobileNavBar(
     '.nav-list li',
 );
 mobileNavbar.init();
+
+// =========== ANIMATED TEXT ===========
+const text = document.querySelector('.text-wrapper');
+text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.text-wrapper .letter',
+    scale: [0, 1],
+    duration: 2500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  })
+
+const text2 = document.querySelector('.home-subtitle');
+text2.innerHTML = text2.textContent.replace(/\S/g, "<span class='letter2'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.home-subtitle .letter2',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2200,
+    delay: (el, i) => 500 + 30 * i
+  })
+
+const text3 = document.querySelector('.home-description');
+text3.innerHTML = text3.textContent.replace(/\S/g, "<span class='letter3'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.home-description .letter3',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  })
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+
+  const hiddenxElements = document.querySelectorAll('.hiddenx');
+  hiddenxElements.forEach((el) => observer.observe(el));
+
+  const hiddensElements = document.querySelectorAll('.hiddens');
+  hiddensElements.forEach((el) => observer.observe(el));
